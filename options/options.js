c4827d2358afcc133f9bdc89db500a0229376663
Crowdbotics <team@crowdbotics.com>
2023-07-08 19:00:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-snow-42006",
+  url: "https://royal-snow-42006.botics.co",
+  api: "https://royal-snow-42006.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
